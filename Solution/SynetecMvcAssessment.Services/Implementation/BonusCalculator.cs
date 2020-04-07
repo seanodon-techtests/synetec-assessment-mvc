5b31aa8ca3f2e15970320b59b02265f3089b3d19
Sean O'Donnell <sfo@planetinnovation.com.au>
2020-04-07 15:10:44 +0100

    Throw excpetion on invalid data

--- Solution/SynetecMvcAssessment.Services/Implementation/BonusCalculator.cs
+++ Solution/SynetecMvcAssessment.Services/Implementation/BonusCalculator.cs
@@ -1,9 +1,17 @@
-﻿namespace SynetecMvcAssessment.Services.Implementation
+﻿using System;
+
+namespace SynetecMvcAssessment.Services.Implementation
 {
 	public class BonusCalculator : IBonusCalculator
 	{
 		public decimal GetBonusForEmployee(decimal employeeSalary, decimal totalSalary, decimal totalBonusPool)
 		{
+			if (employeeSalary < 0 || totalSalary < 0 || totalBonusPool < 0)
+				throw new InvalidOperationException("All fields must be greater than zero!");
+
+			if (employeeSalary > totalSalary)
+				throw new InvalidOperationException("Employee salary cannot be higher than total salary!");
+
 			var bonusPercentage = employeeSalary / totalSalary;
 			var bonusAllocation = bonusPercentage * totalBonusPool;
 
